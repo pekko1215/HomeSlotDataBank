4236d73c6d743955e4b1338700d84992a2372eec
pekko1215 <pekko1215@yahoo.co.jp>
2017-12-28 02:36:45 +0900

    easyform

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const Sequelize = require('sequelize');
+const app = express();
+
+app.use(require('cookie-parser')());
+app.use(require('express-session')({
+	secret:getEnv("SESSION_SECRET"),
+	resave:false,
+	saveUninitialized:false
+}))
+
+const sequelize = new Sequelize(getEnv("DATABASE_URL"))
+
+
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use('/user',function(req,res,next){
+	if(req.session.user){
+		next();
+	}else{
+		res.redirect('/login')
+	}
+})
+app.use("/login",express.static(__dirname + '/public/login'))
+app.use("/",express.static(__dirname + '/public/main'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-});
+
+
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 });
+
+function getEnv(key){
+	return process.env[key]||require('./config.js')[key]
+}
---
+++ routes/login.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('login');
+});
+
+router.post('/', function(req, res, next) {
+    // ここではユーザ名の有無判断のみ行っている
+    // パスワード認証の場合はユーザ名、パスワードの有無判断に加えて
+    // データストア(DBなど)内の存在チェックを合わせて行う
+    if (("userName" in req.body)&&("password" in req.body)) {
+        // セッションへの格納処理
+        // ここでは入力されたユーザ名だけだが、処理要件に応じて
+        // DBから取得した値など（ユーザごとの設定値とか）を格納する
+        req.session.user = { name: req.body.userName };
+        res.redirect('../');
+    } else {
+        var err = '入力が正しくありません。確認して再入力してください。';
+        res.render('login', { error: err });
+    }
+});
+
+module.exports = router;
