617e1f4ee4d46edfa09b6daa783694046a9236fc
pekko1215 <pekko1215@yahoo.co.jp>
2018-01-03 16:16:09 +0900

    userpage

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,8 +1,17 @@
 var router = require('express').Router();
 var path = require('path');
 
+function isAuthenticated(req, res, next){
+    if (req.isAuthenticated()) {  // 認証済
+        return next();
+    }
+    else {  // 認証されていない
+        res.redirect('/login');  // ログイン画面に遷移
+    }
+}
+
 module.exports = function(User) {
-    router.get('/', function(req, res, next) {
+    router.get('/',isAuthenticated, function(req, res, next) {
         res.render('user/dashbord');
     });
     return router;
