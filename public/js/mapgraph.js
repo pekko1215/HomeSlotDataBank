1005d937d19708ea0be52b89dd5c8c9255d0961c
pekko1215 <pekko1215@yahoo.co.jp>
2018-01-13 11:05:27 +0900

    blank

--- public/js/mapgraph.js
+++ public/js/mapgraph.js
@@ -4,18 +4,18 @@ MapGraph = function(_data) {
         width: 676,
         height: 130
     })
-    const Max = Math.max(..._data.datas.reduce((c,d)=>{
-		var stmp = (new Date(d.date)).toLocaleDateString()
-		var idx = c.findIndex(p=>p.date==stmp);
-		if(idx!=-1){
-			c[idx].value += d.value;
-		}else{
-			c.push({value:d.value,date:stmp});
-		}
-		return c
-    },[]).map(d=>d.value));
-    _data.colors = _data.colors.map(color=>{
-		return isNaN(color)?color:'#'+color.toString(16)
+    const Max = Math.max(..._data.datas.reduce((c, d) => {
+        var stmp = (new Date(d.date)).toLocaleDateString()
+        var idx = c.findIndex(p => p.date == stmp);
+        if (idx != -1) {
+            c[idx].value += d.value;
+        } else {
+            c.push({ value: d.value, date: stmp });
+        }
+        return c
+    }, []).map(d => d.value));
+    _data.colors = _data.colors.map(color => {
+        return isNaN(color) ? color : '#' + color.toString(16)
     })
     window.addEventListener('load', () => {
         const $target = document.querySelector(_data.selector);
@@ -51,38 +51,38 @@ MapGraph = function(_data) {
                 var $rect = document.createElementNS(NS, 'rect');
                 $rect.dataset.idx = idx;
                 $rect.dataset.day = k;
-                $rect.dataset.month = i+1
+                $rect.dataset.month = i + 1
                 $rect.setAttribute('width', 10);
                 $rect.setAttribute('height', 10);
 
                 $rect.setAttribute('fill', _data.colors[0]);
-                $rect.setAttribute('x',xidx)
-                $rect.setAttribute('y',12*day)
+                $rect.setAttribute('x', xidx)
+                $rect.setAttribute('y', 12 * day)
                 $g.appendChild($rect)
-                if(day==6){
-					$g = document.createElementNS(NS, 'g');
-					xidx--;
-					weekidx++;
-					$g.setAttribute('transform', `translate(${weekidx * 13},0)`)
-					$allwrap.appendChild($g);
+                if (day == 6) {
+                    $g = document.createElementNS(NS, 'g');
+                    xidx--;
+                    weekidx++;
+                    $g.setAttribute('transform', `translate(${weekidx * 13},0)`)
+                    $allwrap.appendChild($g);
                 }
                 idx++;
                 day = (day + 1) % 7;
             }
         }
-        _data.datas.forEach(data=>{
-			var nowyear = (new Date).getFullYear();
-			var ddate = new Date(data.date);
-			if(ddate.getFullYear() < nowyear){
-				return;
-			}
-			var $rect = document.querySelector(`rect[data-day="${ddate.getDate()}"][data-month="${ddate.getMonth()+1}"]`)
-			var value = parseInt($rect.dataset.value) || 0;
-			value += data.value;
-			var level = parseInt(value/Max*(_data.colors.length-2))+1;
-			$rect.setAttribute('fill',_data.colors[level])
-			$rect.dataset.value = value;
-			console.log($rect)
-		})
+        _data.datas.forEach(data => {
+            var nowyear = (new Date).getFullYear();
+            var ddate = new Date(data.date);
+            if (ddate.getFullYear() < nowyear) {
+                return;
+            }
+            var $rect = document.querySelector(`rect[data-day="${ddate.getDate()}"][data-month="${ddate.getMonth()+1}"]`)
+            var value = parseInt($rect.dataset.value) || 0;
+            value += data.value;
+            var level = parseInt(value / Max * (_data.colors.length - 2)) + 1;
+            $rect.setAttribute('fill', _data.colors[level])
+            $rect.dataset.value = value;
+            console.log($rect)
+        })
     })
 }
