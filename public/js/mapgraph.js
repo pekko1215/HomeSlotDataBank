2bb884df0b698a36c446b6b2a8018928f83afe5f
pekko1215 <pekko1215@yahoo.co.jp>
2018-01-14 18:22:20 +0900

    datatableswidth

--- public/js/mapgraph.js
+++ public/js/mapgraph.js
@@ -1,8 +1,11 @@
 MapGraph = function(_data) {
     const NS = "http://www.w3.org/2000/svg"
+    console.log(_data)
     _data = Object.assign(_data, {
         width: 676,
-        height: 130
+        height: 130,
+        weeknames: "日月火水木金土".split(''),
+        colors: ['#ebedf0', '#c6e48b', '#7bc96f', '#239a3b', '#196127']
     })
     const Max = Math.max(..._data.datas.reduce((c, d) => {
         var stmp = (new Date(d.date)).toLocaleDateString()
@@ -44,6 +47,12 @@ MapGraph = function(_data) {
             date.setMonth(i + 1);
             date.setDate(0);
             var d = date.getDate();
+            var $text = document.createElementNS(NS, 'text');
+
+            $text.innerHTML = `${i+1}月`;
+            $text.setAttribute('x', 12 * weekidx + 1 + 10);
+            $text.setAttribute('font-size', 10)
+            $allwrap.appendChild($text)
             $allwrap.appendChild($g);
             date.setDate(1);
             var day = date.getDay();
@@ -70,7 +79,7 @@ MapGraph = function(_data) {
                 day = (day + 1) % 7;
             }
         }
-        _data.datas.forEach(data => {
+        _data.datas.forEach((data, i) => {
             var nowyear = (new Date).getFullYear();
             var ddate = new Date(data.date);
             if (ddate.getFullYear() < nowyear) {
@@ -79,10 +88,61 @@ MapGraph = function(_data) {
             var $rect = document.querySelector(`rect[data-day="${ddate.getDate()}"][data-month="${ddate.getMonth()+1}"]`)
             var value = parseInt($rect.dataset.value) || 0;
             value += data.value;
+            if ($rect.dataset.idxarr) {
+                $rect.dataset.idxarr += ',' + i
+            } else {
+                $rect.dataset.idxarr = '' + i;
+            }
             var level = parseInt(value / Max * (_data.colors.length - 2)) + 1;
             $rect.setAttribute('fill', _data.colors[level])
             $rect.dataset.value = value;
-            console.log($rect)
+
+        })
+        for (var i = 0; i < 7; i++) {
+            var $text = document.createElementNS(NS, 'text');
+            $text.innerHTML = _data.weeknames[i];
+            $text.setAttribute('dx', 0);
+            $text.setAttribute('dy', 8 + 12 * i);
+            $text.setAttribute('font-size', 10)
+            $allwrap.appendChild($text)
+        }
+        document.querySelectorAll('rect[data-day][data-month]').forEach($rect => {
+            var $dispelm;
+            var hide = function(e) {
+                if ($dispelm) {
+                    try {
+                        document.body.removeChild($dispelm);
+                    } catch (e) {}
+                    $dispelm = null;
+                }
+            }
+            var disp = function(e) {
+                if ($dispelm) {
+                    try {
+                        document.body.removeChild($dispelm);
+                    } catch (e) {}
+                    $dispelm = null;
+                }
+                $dispelm = document.createElement('div')
+                $dispelm.style.position = 'absolute';
+                $dispelm.style.left = e.clientX + document.body.scrollLeft;
+                $dispelm.style.top = e.clientY + document.body.scrollTop + 10;
+                $dispelm.style.background = '#ccc'
+                if (this.dataset.idxarr) {
+                    $dispelm.innerHTML = `${(new Date).getFullYear()}/${this.dataset.month}/${this.dataset.day}<br>`
+                    this.dataset.idxarr.split(',').forEach(idx => {
+                        $dispelm.innerHTML += _data.datas[idx].html;
+                    })
+                    document.body.appendChild($dispelm);
+                }
+            }
+            $rect.addEventListener('mouseover', disp);
+            $rect.addEventListener('mouseout', hide);
+            if (_data.events) {
+                Object.keys(_data.events).forEach(ev => {
+                    $rect.addEventListener(ev, _data.events[ev]);
+                })
+            }
         })
     })
 }
